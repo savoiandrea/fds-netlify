8c98631d1518c118990231c949022c62199b5aa8
Andrea Savoia <andrea.savoia23@yahoo.it>
2023-12-12 00:02:07 +0100

    homepage contents placed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import lottie from "astro-integration-lottie";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,5 +15,6 @@ export default defineConfig({
     }),
     mdx(),
     sitemap(),
+    lottie(),
   ],
 });
